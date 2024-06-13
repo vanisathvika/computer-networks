a6924d3b5611048cbdec05b60cbafd327710e3fb
VANI SATHVIKA REDDY <153749239+vanisathvika@users.noreply.github.com>
2024-06-13 14:07:38 +0530

    Update week7.c

--- week7.c
+++ week7.c
@@ -1,90 +1,39 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <conio.h>
 
-#define EXIT 100
+int main() 
+{ 
+    int i, sn; 
+    int st[20]; 
+    char op; 
 
-#define BITMAP_LEN 2
-#define BYTE_LEN   8
+    printf("\n How many stations: "); 
+    scanf("%d", &sn); 
 
-/* Debug OutPut */
-void printTheDebugOutput(unsigned char *bit_array)
-{
-   int i, j;
+    do 
+    { 
+        printf("\n Enter status of stations\n"); 
+        for (i = 0; i < sn; i++) 
+        { 
+            printf(" Enter status of station %d: ", i + 1); 
+            scanf("%d", &st[i]); 
+        } 
 
-   /* Array is 2 bit_array[0] and [1] */
-   for (i=0 ; i<BITMAP_LEN; i++)
-   {
-     /* Bits 0..7 */
-     for(j=1; j<=BYTE_LEN; j++)
-     {
-        if(bit_array[i] & (1 << (j-1)))
-        {
-           printf("In BIT_MAP[%d] the position of Bit SET : %d\n",
-                                   i, j);
-        }
-     }
-   }
-}
+        // Print ready stations 
+        for (i = 0; i < sn; i++) 
+        { 
+            if (st[i] == 1) 
+            { 
+                printf(" Station %d is ready to transmit\n", i + 1); 
+            } 
+        } 
 
-/*
- *  Main() function
- */
-int main()
-{
+        printf(" Repeat? Press Y :"); 
+        scanf(" %c", &op); 
+    } 
+    while (op == 'y' || op == 'Y'); 
 
-  unsigned int bit_position, setOrUnsetBit, ch;
-  unsigned char bit_Map_array_index, shift_index;
+    getch(); 
 
-  /* Declare Bit Array and intialised to Zero
-   * This BIT_MAP is generally assigned to one domain.
-   * Here BIT MAP (Array of Bits) are used for Debugging Mechanism
-   */
-  unsigned char bit_map[BITMAP_LEN] = { 0 };
-
-  /* In Bit- Maps , there are two options,
-   * either  Set (OR |) or  Unset (AND &) bit.
-   */
- do
- {
-    /* Here the Max number of bits is 16 ranging from 1..16 */
-    printf("Enter the Bit position (bit starts from 1 and Ends at 16) \n");
-    scanf("%d", &bit_position);
-
-    /*
-     * Set/Unset the Bit Map indicates which bits you want to enable
-     */
-    printf(" Do you want to set/unset the Bit (1 or 0) \n");
-    scanf("%d", &setOrUnsetBit);
-
-    /* LOGIC as follows */
-    /* Find Out the Index and and as well as Shift Index */
-
-    /* It Give output as 0 or 1 ( for Bit Position 1..16) */
-    bit_Map_array_index = (bit_position-1) / 8;
-
-    /* Always give output as 0...7 ( for Bit Position 1..16)*/
-    shift_index =  (bit_position-1) % 8;
-
-    printf("The bit_position : %d shift Index : %d\n", bit_position, shift_index);
-
-    /* If set is true */
-    if( setOrUnsetBit)
-     {
-       /* Set the Bit Array */
-       bit_map[bit_Map_array_index] |= 1<<shift_index;
-     }
-    else
-     {
-        /* Clear the Bit Array */
-        bit_map[bit_Map_array_index] &= ~(1<<shift_index);
-     }
-    printf(" The Bit MAP Array : %d\n", bit_map[bit_Map_array_index]);
-    printTheDebugOutput(bit_map);
-
-    printf(" Do You want to Continue then Enter any Number"
-           "and for Exit then enter 100\n");
-    scanf("%d", &ch);
-
-  }while(ch != 100);
- return 0;
+    return 0; 
 }
