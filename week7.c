0951244955f3b97fcacfe78fedb1f5fd75f3ce32
VANI SATHVIKA REDDY <153749239+vanisathvika@users.noreply.github.com>
2024-06-23 09:07:01 +0530

    week7.c

--- week7.c
+++ week7.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <conio.h>
-
 int main() 
 { 
     int i, sn; 
     int st[20]; 
     char op; 
-
     printf("\n How many stations: "); 
     scanf("%d", &sn); 
-
     do 
     { 
         printf("\n Enter status of stations\n"); 
@@ -17,9 +14,7 @@ int main()
         { 
             printf(" Enter status of station %d: ", i + 1); 
             scanf("%d", &st[i]); 
-        } 
-
-        // Print ready stations 
+        }  
         for (i = 0; i < sn; i++) 
         { 
             if (st[i] == 1) 
@@ -32,8 +27,6 @@ int main()
         scanf(" %c", &op); 
     } 
     while (op == 'y' || op == 'Y'); 
-
     getch(); 
-
     return 0; 
 }
